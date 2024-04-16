2eb3a5fe8109a99a9e3f28eebd6f62c071eb95c8
root <root@DESKTOP-62FBA02.localdomain>
2024-04-16 17:22:05 +0900

    Committing changes before revert

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -1,4 +1,7 @@
-<<<<<<< HEAD
+ef3d84 (Committing changes before revert)
+import React from "react";
+
+
 import React from "react";
 
 function AddItem() {
@@ -6,12 +9,4 @@ function AddItem() {
 }
 
 export default AddItem;
-=======
-import React from "react";
 
-function AddItem() {
-  return <div>add!</div>;
-}
-
-export default AddItem;
->>>>>>> 466934e (다시 pr올리기)
