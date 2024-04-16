e7491691fa640108143882e10471d2e6e37f6fc2
Darkthos <kiki7951@naver.com>
2024-04-16 19:31:10 +0900

    Revert "Revert "[김민재] Sprint5""
    This reverts commit 8de82c441d6dec966ce555c3353c4c80ab514af0.

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 function AddItem(props) {
@@ -5,3 +6,12 @@ function AddItem(props) {
 }
 
 export default AddItem;
+=======
+import React from "react";
+
+function AddItem(props) {
+  return <div>Add!!</div>;
+}
+
+export default AddItem;
+>>>>>>> parent of 8de82c4 (Revert "[김민재] Sprint5")
