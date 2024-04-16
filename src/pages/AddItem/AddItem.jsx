ce65e7cfe1e47f45237a9a8dcaeb693336c5636b
Darkthos <kiki7951@naver.com>
2024-04-16 14:00:20 +0900

    다시 pr올리기

--- src/pages/AddItem/AddItem.jsx
+++ src/pages/AddItem/AddItem.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 
 function AddItem() {
@@ -5,3 +6,12 @@ function AddItem() {
 }
 
 export default AddItem;
+=======
+import React from "react";
+
+function AddItem() {
+  return <div>add!</div>;
+}
+
+export default AddItem;
+>>>>>>> 466934e (다시 pr올리기)
